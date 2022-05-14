cb454ab627d3e34bce02e775b6b5f529dbe5e8ba
nate <nathangchay@gmail.com>
2022-05-14 15:40:17 -0600

    Create 62 - Unique Paths.cpp

---
+++ medium/62 - Unique Paths.cpp
@@ -0,0 +1,25 @@
+/*
+  62. Unique Paths - Medium
+
+  There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e.,
+  grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+  Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+  The test cases are generated so that the answer will be less than or equal to 2 * 109.
+*/
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int N = n + m - 2;
+        int r = min(n, m) - 1;
+        double result = 1;
+        
+        for (int i = 1; i <= r; ++i, N--) {
+            result = result * N / i;
+        }
+        
+        return (int)res;
+    }
+};
