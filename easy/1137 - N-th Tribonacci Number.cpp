1c5dc308349c10838a8449b30b1d7be26b855dbc
nate <nathangchay@gmail.com>
2022-05-14 15:36:17 -0600

    Create 1137 - N-th Tribonacci Number.cpp

---
+++ easy/1137 - N-th Tribonacci Number.cpp
@@ -0,0 +1,28 @@
+/*
+  1137. N-th Tribonacci Number - Easy
+
+  The Tribonacci sequence Tn is defined as follows: 
+
+  T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+
+  Given n, return the value of Tn.
+*/
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n < 2) return n;
+        
+        vector<int> dp(n + 1, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+        } 
+        
+        return dp[n];
+    }
+};
