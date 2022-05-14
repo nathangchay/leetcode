f2d3fd5225a4d675ad518b61ea8d950c8c9e5288
nate <nathangchay@gmail.com>
2022-05-14 15:37:04 -0600

    Create 119 - Pascal's Triangle II.cpp

---
+++ easy/119 - Pascal's Triangle II.cpp
@@ -0,0 +1,28 @@
+/*
+  119. Pascal's Triangle II - Easy
+
+  Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+  In Pascal's triangle, each number is the sum of the two numbers directly above it.
+*/
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> dp(rowIndex + 1);
+        
+        dp[0] = {1};
+        
+        for (int i = 1; i <= rowIndex; i++) {
+            dp[i].push_back(1);
+            
+            for (int j = 1; j < i; j++) {
+                dp[i].push_back(dp[i - 1][j - 1] + dp[i - 1][j]);
+            }
+            
+            dp[i].push_back(1);
+        }
+        
+        return dp[rowIndex];
+    }
+};
