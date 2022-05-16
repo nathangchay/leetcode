225b2091ab809013f96836a8892aad386d1927b9
nate <nathangchay@gmail.com>
2022-05-16 15:29:28 -0600

    Create 94 - Binary Tree Inorder Traversal.cpp

---
+++ easy/94 - Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,41 @@
+/*
+  94. Binary Tree Inorder Traversal - Easy
+
+  Given the root of a binary tree, return the inorder traversal of its nodes' values.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> result;
+    
+    void traverse(TreeNode* node) {
+        if (node->left != nullptr) {
+            traverse(node->left);
+        }
+        
+        result.push_back(node->val);
+        
+        if (node->right != nullptr) {
+            traverse(node->right);
+        }
+    }
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+        if (root != nullptr) {
+            traverse(root);
+        }
+        
+        return result;
+    }
+};
