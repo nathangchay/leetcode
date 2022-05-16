26ad5c8102eef7cdbc970f4ec7166553e354ca00
nate <nathangchay@gmail.com>
2022-05-16 15:29:24 -0600

    Create 66 - Plus One.cpp

---
+++ easy/66 - Plus One.cpp
@@ -0,0 +1,31 @@
+/*
+  66. Plus One - Easy
+
+  You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant
+  to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+  Increment the large integer by one and return the resulting array of digits.
+*/
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        
+        digits[n - 1]++;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] > 9) {
+                digits[i] = 0;
+                
+                if (i > 0) {
+                    digits[i - 1]++;
+                } else {
+                    digits.insert(digits.begin(), 1);
+                }
+            }
+        }
+        
+        return digits;
+    }
+};
